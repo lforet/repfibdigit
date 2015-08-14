9ab24873dcafacafb4990b5efd32c017da779ce0
Laird Foret <laird@isotope11.com>
2015-08-14 15:14:05 -0500

    add setup script - run as sudo sh

---
+++ run_to_setup_dev_env.py
@@ -0,0 +1,8 @@
+
+
+apt-get update
+apt-get upgrade
+apt-get install -y python-pycuda
+
+
+
---
+++ setup_dev_end.sh
@@ -0,0 +1,8 @@
+apt-get update
+apt-get -y upgrade
+apt-get install -y cuda
+apt-get install -y python-pycuda
+
+
+
+
